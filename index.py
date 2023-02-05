6285361cfd90d41f678df9a4d956358c050b9a61
RyannKim327 <rksesgundo123@gmail.com>
2023-02-05 19:45:59 +0800

    02-05-23 19:45

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 import json
 
+# File import and load as JSON
 def jsonFile(filename):
 	file = open(filename)
 	return json.loads(file.read())
 
+
+# String execution line by line
 def execution(program):
 	for i in program:
-		print(eval(i))
+		eval(i)
 
 
 data = jsonFile("index.json")
-# print(data['code'])
 execution(data['code'])
